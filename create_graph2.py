1a7fd300fc8007eebfbaa8786010662dc5e43d1c
Hugo Sandelius <hugo.sandelius@gmail.com>
2016-02-29 13:49:01 +0100

    nodes must now reciprocate edges in rng for edge to be created in globl graph

--- create_graph2.py
+++ create_graph2.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import pydsm
 import queue
+from collections import defaultdict
 
 
 # TODO: if queue is empty, load next unvisited word from model into queue and restart
@@ -10,6 +11,8 @@ def create_graph(model, root, gpickle=None):
     q = queue.Queue()
 
     visited = set()
+
+    potential_edges = defaultdict(list)
 
     q.put(root)
     visited.add(root)
@@ -22,11 +25,15 @@ def create_graph(model, root, gpickle=None):
         rng = model.relative_neighborhood(word, 20, format='networkx')
         
         if gpickle:
-            if n % 1000 == 0:
+            if n % 100 == 0:
                 nx.write_gpickle(g, gpickle)
+                print("Saving graph to file:", gpickle, "(iteration:", n, ", nodes:", str(g.number_of_nodes()) + ")")
 
         for neighbor, labels in rng[word].items():
-            g.add_edge(word, neighbor, labels)
+            if word in potential_edges[neighbor]:
+                g.add_edge(word, neighbor, labels)
+            else:
+                potential_edges[word].append(neighbor)
             if neighbor not in visited:
                 q.put(neighbor)
                 visited.add(neighbor)
