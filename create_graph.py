25b9d972d7b272b7f807482d19e5db408b5584cb
Hugo Sandelius <hugo.sandelius@gmail.com>
2016-03-01 13:05:12 +0100

    create_graph now creates graph containing all entities in model, instead of only entities reachable from root

--- create_graph.py
+++ create_graph.py
@@ -2,13 +2,13 @@ import networkx as nx
 import pydsm
 import queue
 from collections import defaultdict
+from itertools import chain
 
 
 def nearest_indices(model, word, neighbor, n=1):
     nin = model.nearest_index_neighbors(model[word] + model[neighbor])
     return ", ".join(nin[i][1] for i in range(n))
 
-# TODO: if queue is empty, load next unvisited word from model into queue and restart
 
 def create_graph(model, root, save_file=None, save_every_nth_iteration=1000, label_function=None):
     g = nx.Graph()
@@ -18,30 +18,38 @@ def create_graph(model, root, save_file=None, save_every_nth_iteration=1000, lab
 
     potential_edges = defaultdict(list)
 
+    first_time = True
+
     q.put(root)
     visited.add(root)
 
     n = 0
 
-    while not q.empty():
-        word = q.get()
+    for word_vec in chain([None], model):
+        if not first_time:
+            root = word_vec.row2word[0]
+        first_time = False
 
-        rng = model.relative_neighborhood(word, 20, format='networkx')
-        
-        if save_file:
-            if n % save_every_nth_iteration == 0:
-                nx.write_gpickle(g, save_file)
-                print("Saving graph to file: '" + save_file + "' (iteration: " + str(n) + ", nodes: " + str(g.number_of_nodes()) + ")")
+        if root not in visited:
+            while not q.empty():
+                word = q.get()
 
-        for neighbor, labels in rng[word].items():
-            if word in potential_edges[neighbor]:
-                if label_function is not None:
-                    new_label = label_function(word, neighbor)
-                g.add_edge(word, neighbor, l=new_label)
-            else:
-                potential_edges[word].append(neighbor)
-            if neighbor not in visited:
-                q.put(neighbor)
-                visited.add(neighbor)
+                rng = model.relative_neighborhood(word, 20, format='networkx')
+                
+                if save_file:
+                    if n % save_every_nth_iteration == 0:
+                        nx.write_gpickle(g, save_file)
+                        print("Saving graph to file: '" + save_file + "' (iteration: " + str(n) + ", nodes: " + str(g.number_of_nodes()) + ")")
 
-        n += 1
+                for neighbor, labels in rng[word].items():
+                    if word in potential_edges[neighbor]:
+                        if label_function is not None:
+                            new_label = label_function(word, neighbor)
+                        g.add_edge(word, neighbor, l=new_label)
+                    else:
+                        potential_edges[word].append(neighbor)
+                    if neighbor not in visited:
+                        q.put(neighbor)
+                        visited.add(neighbor)
+
+                n += 1
